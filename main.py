8e0d1b4479d605da3435c78de5c3db56970ddac4
TaylorMaide <dylancopeland89@gmail.com>
2024-11-03 13:01:27 -0500

    Making a player

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import pygame
+
 from constants import *
+from player import *
+
 def main():
+
 	pygame.init()
+
+	my_clock = pygame.time.Clock()
+	dt = 0
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0, 0, 0))
+		player.draw(screen)
 		pygame.display.flip()
+		dt = my_clock.tick(60) / 1000 
 
 if __name__ == "__main__":
 	main()
